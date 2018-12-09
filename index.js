7b1b38b0bbec54c0dede70aa2f1695bdb5477bbd
Aaron Leo Cooper <aaronleocooper@gmail.com>
2018-12-09 15:51:35 +0800

    feat: Add eslint import plugin

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 module.exports = {
-  extends: ['netflix-dea'],
+  extends: [
+    'netflix-dea',
+    'plugin:import/errors'
+  ],
   rules: {
-    quotes: [2, 'single'],
-    semi: [2, 'always'],
-    curly: [2, 'all'],
+    'quotes': [2, 'single'],
+    'semi': [2, 'always'],
+    'curly': [2, 'all'],
     'array-bracket-spacing': [2, 'never'],
-    'semi-style': [2, 'last']
+    'semi-style': [2, 'last'],
+    'no-extra-parens': [2, 'all', {
+      'ignoreJSX': 'multi-line',
+      'nestedBinaryExpressions': false
+    }]
   }
 };
